6c57d695df8f660ff2f6689d3c4ab05e8c624503
Matthew Clark <afromatt6288@hotmail.com>
2023-02-04 22:16:35 -0700

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(drivers, toMatch) {
+    return drivers.filter(function(name) {
+        if (name.toLowerCase() === toMatch.toLowerCase()) {
+            return name
+        }
+    })
+}
+
+function fuzzyMatch(drivers, firstLetterToMatch) {
+    return drivers.filter(function(name) {
+        if (name.toLowerCase().indexOf(firstLetterToMatch.toLowerCase()) === 0) {
+            return name
+        }
+    })
+}
+
+function matchName (drivers, toMatch) {
+    return drivers.filter(function(source) {
+        if (source.name === toMatch) {
+            return ("name", "hometown")
+        }
+    })
+
+}
+
+// function matchName(source, soughtName) {
+//     return source.filter((record) => record.name === soughtName);
+//   }
